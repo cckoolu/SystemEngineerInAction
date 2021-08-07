3e9f0a79dc8606e444e19c1f37c5274a4e1e4ab9
cckoolu <cckoolu@gmail.com>
2021-08-07 10:46:59 +0800

    修改格式

--- build.sh
+++ build.sh
@@ -16,3 +16,6 @@ bundle-2.7 exec rake book:build_html
 sed -i 's#https://cdnjs.cloudflare.com/ajax/libs/highlight.js/.*/styles/github.min.css#css/github.min.css#' 小盧的自我修养.html
 sed -i 's#https://cdnjs.cloudflare.com/ajax/libs/highlight.js/.*/highlight.min.js#js/highlight.min.js#' 小盧的自我修养.html
 sed -i 's#https://cdnjs.cloudflare.com/ajax/libs/mathjax/.*/MathJax.js#js/MathJax/MathJax.js#' 小盧的自我修养.html
+
+
+cp ./小盧的自我修养.html /home/lu/workSpace/cckoolu.github.io/seia/index.html
